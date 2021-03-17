a363f4d03617d989127d3a7fc17d98550f85d89e
nghia.ngo <nghia.ngo@ubitec.com>
2021-03-18 01:56:54 +0700

    Using TestBed to create environment for component

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,109 +1,125 @@
+import { HeroService } from './../hero.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
 import { HeroesComponent } from './heroes.component';
+import { By } from '@angular/platform-browser';
 describe('HerosComponent', () => {
-  let fixture: HeroesComponent;
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
   let heroServiceMock: any;
 
   beforeEach(() => {
     heroServiceMock = {
       getHeroes: jest.fn(),
       addHero: jest.fn(),
-      deleteHero: jest.fn()
-    }
-    fixture = new HeroesComponent(heroServiceMock);
-  })
+      deleteHero: jest.fn(),
+    };
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      declarations: [HeroesComponent],
+      providers: [{ provide: HeroService, useValue: heroServiceMock }],
+    }).compileComponents();
+
+    heroServiceMock.getHeroes.mockReturnValue(of([]));
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('DOM testing', () => {
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
+    describe('hero button', () => {
+      let addHeroButton: DebugElement;
+      beforeEach(() => {
+        addHeroButton = fixture.debugElement.query(By.css('button.add-button'));
+      });
+
+      it('should have add hero button', () => {
+        expect(addHeroButton).not.toBeNull();
+      });
+      it('should invoke add function when trigger button add hero', () => {
+        const addSpy = spyOn(component, 'add');
+        addHeroButton.nativeElement.click();
+        expect(addSpy).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('snapshot of markup', () => {
+    it('should match the markup for a item', () => {
+      fixture.detectChanges();
+      expect(fixture).toMatchSnapshot();
+    });
+  });
 
   describe('ngOnInit', () => {
     it('should call getHeros', () => {
-      const getHeroesSpy = jest.spyOn(fixture, 'getHeroes');
-      heroServiceMock.getHeroes.mockReturnValue(of([]))
-      fixture.ngOnInit();
+      const getHeroesSpy = jest.spyOn(component, 'getHeroes');
+      heroServiceMock.getHeroes.mockReturnValue(of([]));
+      component.ngOnInit();
       expect(getHeroesSpy).toHaveBeenCalled();
-    })
+    });
   });
 
   describe('Function testing', () => {
     beforeEach(() => {
-      heroServiceMock.getHeroes.mockReturnValue(of([]))
-      fixture.ngOnInit();
+      heroServiceMock.getHeroes.mockReturnValue(of([]));
+      component.ngOnInit();
     });
 
     describe('add function', () => {
       it('add new not work when name is empty', () => {
         const name = '';
-        const addSpy = jest.spyOn(fixture, 'add');
-        fixture.add(name);
+        const addSpy = jest.spyOn(component, 'add');
+        component.add(name);
         expect(addSpy).toHaveBeenCalled();
         expect(heroServiceMock.addHero).not.toHaveBeenCalled();
       });
 
       it('add new work when name has value', () => {
         const name = 'a new hero';
-        const addSpy = jest.spyOn(fixture, 'add');
-        const hero = {id: 1, name: name};
+        const addSpy = jest.spyOn(component, 'add');
+        const hero = { id: 1, name: name };
         heroServiceMock.addHero.mockReturnValue(of(hero));
 
-        fixture.add(name);
+        component.add(name);
         expect(addSpy).toHaveBeenCalled();
         expect(heroServiceMock.addHero).toHaveBeenCalled();
-        expect(fixture.heroes).toEqual([hero]);
+        expect(component.heroes).toEqual([hero]);
       });
     });
 
     describe('delete Function', () => {
       const name = 'a new hero';
-      let hero = {id: 1, name: name};
+      let hero = { id: 1, name: name };
       beforeEach(() => {
-        heroServiceMock.getHeroes.mockReturnValue(of([]))
-        fixture.ngOnInit();
+        heroServiceMock.getHeroes.mockReturnValue(of([]));
+        component.ngOnInit();
         heroServiceMock.addHero.mockReturnValue(of(hero));
-        fixture.add(name);
+        component.add(name);
         heroServiceMock.deleteHero.mockReturnValue(of());
       });
       it('should remove hero when hero is found', () => {
-        const deleteSpy = jest.spyOn(fixture, 'delete');
-        fixture.delete(hero);
+        const deleteSpy = jest.spyOn(component, 'delete');
+        component.delete(hero);
 
         expect(deleteSpy).toHaveBeenCalled();
-        expect(fixture.heroes.length).toEqual(0);
-
+        expect(component.heroes.length).toEqual(0);
       });
       it('should not remove hero when hero is not found', () => {
-        const deleteSpy = jest.spyOn(fixture, 'delete');
-        hero = {...hero, name:'other'};
-        fixture.delete(hero);
+        const deleteSpy = jest.spyOn(component, 'delete');
+        hero = { ...hero, name: 'other' };
+        component.delete(hero);
 
         expect(deleteSpy).toHaveBeenCalled();
-        expect(fixture.heroes.length).toEqual(1);
-      });
-    });
-
-        describe('delete Function', () => {
-      const name = 'a new hero';
-      let hero = {id: 1, name: name};
-      beforeEach(() => {
-        heroServiceMock.getHeroes.mockReturnValue(of([]))
-        fixture.ngOnInit();
-        heroServiceMock.addHero.mockReturnValue(of(hero));
-        fixture.add(name);
-        heroServiceMock.deleteHero.mockReturnValue(of());
-      });
-      it('should remove hero when hero is found', () => {
-        const deleteSpy = jest.spyOn(fixture, 'delete');
-        fixture.delete(hero);
-
-        expect(deleteSpy).toHaveBeenCalled();
-        expect(fixture.heroes.length).toEqual(0);
-
-      });
-      it('should not remove hero when hero is not found', () => {
-        const deleteSpy = jest.spyOn(fixture, 'delete');
-        hero = {...hero, name:'other'};
-        fixture.delete(hero);
-
-        expect(deleteSpy).toHaveBeenCalled();
-        expect(fixture.heroes.length).toEqual(1);
+        expect(component.heroes.length).toEqual(1);
       });
     });
   });
-})
+});
